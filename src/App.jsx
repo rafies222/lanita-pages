4a11465a187cceef26ca4d8070fc6251faaa1281
Rafael Vieira <j.rafaelvpinheiro@gmail.com>
2025-04-25 19:07:27 -0300

    Adicionando o VariosComponents.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-
+import { Component_01, Component_02 } from './components/VariosComponents';
 
 const App = () => {
   return (  
@@ -10,9 +10,10 @@ const App = () => {
       <Header/>
       <Main/>
       <Footer/>
+      <Component_01/>
+      <Component_02/>
     </div>
   );
 }
  
 export default App;
-<Header/>
---
+++ src/components/VariosComponents.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export const Component_01 = () =>{
+    return (
+        <div className="borboleta">🦋</div>
+    );
+}
+
+export const Component_02 = () => {
+    return(
+        <a class="siteOficial" href="https://www.lanadelrey.com/">Site Oficial da Cantora</a>
+    );
+}
+
